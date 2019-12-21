17c3513c3ac87f39048c4681a53b65782a91918c
onjsdnjs <onjsdnjs@gmail.com>
2019-12-21 11:15:22 +0900

    Initial commit

---
+++ src/test/java/io/security/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
