640f116f4da15cb0f406b50def98a6fe8cbb9726
onjsdnjs <onjsdnjs@gmail.com>
2019-12-21 20:25:04 +0900

    lecture

---
+++ src/main/java/io/security/demo/SecurityConfig.java
@@ -0,0 +1,15 @@
+package io.security.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.anonymous();
+    }
+}
