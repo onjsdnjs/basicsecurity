536588aebc3353d09445c8a1c00554e47b20c332
onjsdnjs <onjsdnjs@gmail.com>
2019-12-22 18:51:17 +0900

    lecture'

--- src/main/java/io/security/demo/SecurityConfig.java
+++ src/main/java/io/security/demo/SecurityConfig.java
@@ -1,49 +1,18 @@
 package io.security.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    AuthenticationManagerBuilder authenticationManagerBuilder;
-
-//    @Bean
-//    @Override
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
-
-    public AuthenticationManager parentAuthenticationManager() {
-        List<AuthenticationProvider> authProviderList = new ArrayList<>();
-        authProviderList.add(new ParentAuthenticationProvider());
-        ProviderManager providerManager = new ProviderManager(authProviderList);
-        return providerManager;
-    }
-
-    public AuthenticationManager childAuthenticationManager() {
-        List<AuthenticationProvider> authProviderList = new ArrayList<>();
-        authProviderList.add(new ChildAuthenticationProvider());
-        ProviderManager providerManager = new ProviderManager(authProviderList, parentAuthenticationManager());
-        authenticationManagerBuilder.parentAuthenticationManager(providerManager);
-        return providerManager;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(new ParentAuthenticationProvider());
     }
 
     protected void configure(HttpSecurity http) throws Exception {
@@ -51,15 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest().authenticated()
         .and()
-                .formLogin()
-        .and()
-                .addFilterBefore(authenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+                .formLogin();
     }
 
-    @Bean
-    public AuthenticationFilter authenticationFilter() {
-        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
-        authenticationFilter.setAuthenticationManager(childAuthenticationManager());
-        return authenticationFilter;
-    }
 }
