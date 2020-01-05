aae80f4ac241a05f0d94e546daadaddc12f86ea2
onjsdnjs <onjsdnjs@gmail.com>
2020-01-05 18:07:29 +0900

    lecture

--- src/main/java/io/security/demo/SecurityConfig.java
+++ src/main/java/io/security/demo/SecurityConfig.java
@@ -12,6 +12,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated();
+                .anyRequest().permitAll();
     }
 }
