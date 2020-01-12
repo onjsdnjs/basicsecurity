7f44333bdec8b138a36d391c99ab4635cd0f3396
leaven <leaven73@gmail.com>
2020-01-12 16:49:24 +0900

    lecture

--- src/main/java/io/security/demo/SecurityConfig.java
+++ src/main/java/io/security/demo/SecurityConfig.java
@@ -4,6 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -12,18 +21,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/loginPage").permitAll()
                 .anyRequest().authenticated()
         .and()
                 .formLogin()
-                .defaultSuccessUrl("/home")
-                .failureUrl("/login.html?error=true")
-                .and()
-                .logout()
-	            .logoutSuccessUrl("/login")
-        .and()
-                .rememberMe()
-                .rememberMeParameter("remember") // Default 는 remember-me
-                .tokenValiditySeconds(3600) // 메모리 저장소 Default 는 14일
-                .key( "springsecurity");
+                .loginPage("/loginPage")
+                .loginProcessingUrl("/login_proc")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login?error=true")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        System.out.println("success");
+                        response.sendRedirect("/");
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                        System.out.println("failure");
+                        response.sendRedirect("/login");
+                    }
+                });
     }
 }
