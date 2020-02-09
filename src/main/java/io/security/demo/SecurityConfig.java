312eccb2ef4ea63a3755fc95bcac0e09d84796ff
leaven <leaven73@gmail.com>
2020-02-09 16:41:00 +0900

    lecture

--- src/main/java/io/security/demo/SecurityConfig.java
+++ src/main/java/io/security/demo/SecurityConfig.java
@@ -15,6 +15,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin();;
+                .formLogin();
     }
 }
