2ae2f0f90526e9a183ef437818f56b248f47c025
onjsdnjs <onjsdnjs@gmail.com>
2020-02-15 20:34:14 +0900

    lecture

--- src/main/java/io/security/demo/SecurityConfig.java
+++ src/main/java/io/security/demo/SecurityConfig.java
@@ -1,19 +1,32 @@
 package io.security.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
                 .anyRequest().authenticated()
         .and()
-                .formLogin();
+                .formLogin()
+        .and()
+                .rememberMe()
+                .userDetailsService(userDetailsService)
+        ;
+
+
+                ;
     }
 }
--- src/main/java/io/security/demo/SecurityController.java
+++ src/main/java/io/security/demo/SecurityController.java
@@ -1,34 +1,19 @@
 package io.security.demo;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String index(){
         return "index";
     }
 
     @GetMapping("/user")
-    public void user(HttpSession session){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println("authentication: " + authentication);
-        System.out.println("user: " + authentication.getName());
-
-        Object securityContextObject = session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
-        Authentication authentication2 = ((SecurityContext)securityContextObject).getAuthentication();
-        System.out.println("authentication: " + authentication2);
-        System.out.println("user: " + authentication.getName());
-
+    public String user(){
+        return "user";
     }
 }
