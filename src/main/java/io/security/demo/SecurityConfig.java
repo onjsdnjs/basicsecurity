932a06fb6330d33c9ab206fefb1ac383214354db
onjsdnjs <onjsdnjs@gmail.com>
2020-03-07 15:40:17 +0900

    lecture'

--- src/main/java/io/security/demo/SecurityConfig.java
+++ src/main/java/io/security/demo/SecurityConfig.java
@@ -1,16 +1,19 @@
 package io.security.demo;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@Order(0)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .antMatcher("/admin/**")
                 .authorizeRequests()
                 .anyRequest().authenticated()
         .and()
@@ -20,12 +23,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
 @Configuration
+@Order(1)
 class SecurityConfig2 extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
                 .and()
                 .formLogin();
     }
---
+++ src/main/java/io/security/demo/SecurityController.java
@@ -0,0 +1,14 @@
+package io.security.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecurityController {
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin page";
+    }
+
+}
